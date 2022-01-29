60e88ddaee45588343ad485011ae9bc2b48d1f7b
Mouleaswar <74062509+M0u1ea5@users.noreply.github.com>
2022-01-29 10:01:46 +0530

    Create Third Wave.py

---
+++ CodeChef/Third Wave.py
@@ -0,0 +1,20 @@
+# Question Link
+# https://www.codechef.com/CDKR2022/problems/WAVE3
+
+import sys
+from math import sqrt,ceil,floor,gcd
+from collections import Counter,defaultdict
+
+def int_arr(): return list(map(int,input().split()))
+def str_arr(): return list(map(str,input().split()))
+def get_str(): return map(str,input().split())
+def get_int(): return map(int,input().split())
+def get_flo(): return map(float,input().split())
+def lcm(a,b): return (a*b) // gcd(a,b)
+
+mod = 1000000007
+
+def solve():pass
+for _ in range(int(input())):
+    n = int(input())
+    print(n*2)
